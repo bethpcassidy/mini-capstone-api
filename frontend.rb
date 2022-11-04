28ce70d8b8fefd972605c73b86ed94cb2a0a35d8
beth <bethcassdy@gmail.com>
2022-11-04 13:37:36 -0400

    ready for website

--- frontend.rb
+++ frontend.rb
@@ -1,9 +1,9 @@
-rquire "http"
+require "http"
 
-option1 = "1 for catalog"
-option2 = "2 for product search"
-option3 = "3 for plushies"
-option4 = "back"
+option1 = "[1] for [catalog]"
+option2 = "[2] for [product search]"
+option3 = "[3] for [plushies]"
+option4 = "[back]"
 input = 0
 
 while input != 3
@@ -13,8 +13,12 @@ while input != 3
   puts option3
   input = gets.chomp
   if input == 1
-    response = HTTP.get("")
+    response = HTTP.get("http://localhost:3000/products/")
   elsif input == 2
+    search = ""
+    puts: "type search here"
+    search = gets.chomp
+
     response = HTTP.get("")
   elsif input == 3
     response = HTTP.get("")
