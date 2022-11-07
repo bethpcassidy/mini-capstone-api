cd83ec112be219a222381623e2b54fe2fc124da5
beth <bethcassdy@gmail.com>
2022-11-07 13:21:54 -0500

    model methods

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,18 @@
 class Product < ApplicationRecord
+  def friendly_created_at
+    created_at.strftime("%B %e, %Y")
+  end
+
+  def is_discounted?
+    if price < 10
+      return true
+    else
+      return false
+    end
+  end
+
+  def tax
+    tax = 0.09 * price
+    price = +tax
+  end
 end
